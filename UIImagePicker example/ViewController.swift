1d715cd1f99a41f68458f7ff9aafb1221f328ea0
Amit Chowdhury <amit@nestedapps.com>
2016-09-28 12:01:51 +0600

    Initial Commit

---
+++ UIImagePicker example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIImagePicker example
+//
+//  Created by Amit Chowdhury on 9/28/16.
+//  Copyright © 2016 Amit Chowdhury. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
