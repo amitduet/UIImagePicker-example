79d94a49582239291181a1344eba339d844743f5
Amit Chowdhury <amit@nestedapps.com>
2016-09-29 11:23:09 +0600

    Image picker functionality add

--- UIImagePicker example/ViewController.swift
+++ UIImagePicker example/ViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+    @IBOutlet weak var backgroundImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func imagePickFromPhotoLib(_ sender: UIButton) {
+        let imagePickerController = UIImagePickerController()
+        // imagePickerController.allowsEditing = true
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = .photoLibrary
+        
+        present(imagePickerController, animated: true, completion: nil)
 
+    }
+
+    @IBAction func imagePickFromCamera(_ sender: UIButton) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .camera
+        imagePickerController.allowsEditing = true
+        imagePickerController.cameraDevice = .front
+        imagePickerController.cameraFlashMode = .auto
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+
+        backgroundImageView.image = selectedImage
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    
 }
 
